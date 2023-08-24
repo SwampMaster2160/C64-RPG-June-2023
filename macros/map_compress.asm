a8dfab62dc702524902dbcfd155e719906f7b51f
SwampMaster2160 <jaxon1454@gmail.com>
2023-08-24 12:34:25 +1200

    110

--- macros/map_compress.asm
+++ macros/map_compress.asm
@@ -1,70 +1,4 @@
-	mac setRecentMetatile
-.in_mod_8 set {1} % 8
-		if .in_mod_8 == 0
-recent_metatile_0 set {1}
-			mexit
-		endif
-		if .in_mod_8 == 1
-recent_metatile_1 set {1}
-			mexit
-		endif
-		if .in_mod_8 == 2
-recent_metatile_2 set {1}
-			mexit
-		endif
-		if .in_mod_8 == 3
-recent_metatile_3 set {1}
-			mexit
-		endif
-		if .in_mod_8 == 4
-recent_metatile_4 set {1}
-			mexit
-		endif
-		if .in_mod_8 == 5
-recent_metatile_5 set {1}
-			mexit
-		endif
-		if .in_mod_8 == 6
-recent_metatile_6 set {1}
-			mexit
-		endif
-recent_metatile_7 set {1}
-	endm
-
-	; Gets the recent metatile with the index {1} % 8 and stores it in recent_metatile
-	mac getRecentMetatile
-.in_mod_8 set {1} % 8
-		if .in_mod_8 == 0
-recent_metatile set recent_metatile_0
-			mexit
-		endif
-		if .in_mod_8 == 1
-recent_metatile set recent_metatile_1
-			mexit
-		endif
-		if .in_mod_8 == 2
-recent_metatile set recent_metatile_2
-			mexit
-		endif
-		if .in_mod_8 == 3
-recent_metatile set recent_metatile_3
-			mexit
-		endif
-		if .in_mod_8 == 4
-recent_metatile set recent_metatile_4
-			mexit
-		endif
-		if .in_mod_8 == 5
-recent_metatile set recent_metatile_5
-			mexit
-		endif
-		if .in_mod_8 == 6
-recent_metatile set recent_metatile_6
-			mexit
-		endif
-recent_metatile set recent_metatile_7
-	endm
-	
+	; Setup the compressor for a new map of metatiles
 	mac newMapMetatiles
 index set 0
 		repeat 8
@@ -74,14 +8,7 @@ index set index + 1
 is_in_run set 0
 	endm
 
-	/*mac mapMetatileRow
-.index set 1
-		repeat 10
-			echo {.index}
-			mapSingleMetatile {.index}
-.index set .index + 1
-		repend
-	endm*/
+	; Compress a row to 10 metatiles
 	mac mapMetatileRow
 		mapSingleMetatile {1}
 		mapSingleMetatile {2}
@@ -95,9 +22,38 @@ is_in_run set 0
 		mapSingleMetatile {10}
 	endm
 
+	; End the compressor for the map and write any data that has not been written for the map
+	mac endMapMetatiles
+		if is_in_run
+is_in_run set 0
+			byte %10000000 | ((run_byte % 8) << 4) | (run_length - 1)
+		endif
+	endm
+
+; ----- Only used within this file -----
+	
+	; Store {1} in the recent metatile list at the index {1} % 8
+	mac setRecentMetatile
+.in_mod_8 set {1} % 8
+		setRecentMetatileIndexVal .in_mod_8, {1}
+	endm
+	
+	mac setRecentMetatileIndexVal
+recent_metatile_{1} set {2}
+	endm
+
+	; Get from the recent metatile list at the index {1} % 8 and store it in recent_metatile
+	mac getRecentMetatile
+.in_mod_8 set {1} % 8
+		getRecentMetatileIndexVal .in_mod_8
+	endm
+
+	mac getRecentMetatileIndexVal
+recent_metatile set recent_metatile_{1}
+	endm
+
 	; Compress a single metatile
 	mac mapSingleMetatile
-		;echo "I", {1}
 		if is_in_run && (run_byte != {1} || run_length == 16)
 			endMapMetatiles
 		endif
@@ -112,16 +68,6 @@ run_length set 1
 			else
 				byte {1}
 				setRecentMetatile {1}
-				;echo "A", {1}
 			endif
-		endif
-	endm
-
-	mac endMapMetatiles
-		if is_in_run
-is_in_run set 0
-			byte %10000000 | ((run_byte % 8) << 4) | (run_length - 1)
-			;echo "B", %10000000 | ((run_byte % 8) << 4) | (run_length - 1)
-			;echo "RB", run_byte, "RBM", (run_byte % 8), "RL", (run_length - 1)
 		endif
 	endm
